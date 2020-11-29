05674847a0a170f63c9d5bd5b9a01e3e0e61de6b
Vasili-K <72854723+Vasili-K@users.noreply.github.com>
2020-11-29 11:53:39 +0100

    feat: add work files
    html, css, js, video

---
+++ controller2.js
@@ -0,0 +1,27 @@
+//variables
+
+let speed = document.querySelector(".speed");
+let bar = document.querySelector(".speed__bar");
+let video = document.querySelector(".flex");
+
+//functions
+let handleMove = function (e) {
+  //if(!isDrawing) return;
+
+  let y = e.pageY - this.offsetTop;
+  let percent = y / this.offsetHeight;
+  let min = 0.4;
+  let max = 4;
+  let height = Math.round(percent * 100) + "%";
+  let playbackRate = percent * (max - min) + min;
+
+  bar.style.height = height;
+  bar.textContent = playbackRate.toFixed(2) + " x";
+
+  video.playbackRate = playbackRate;
+};
+
+//events
+speed.addEventListener("mousemove", handleMove);
+//speed.addEventListener("mousedown", () => (isDrawing = true));
+//speed.addEventListener("mouseup", () => (isDrawing = false));
